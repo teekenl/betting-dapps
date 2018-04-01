a4734333f1b1eeb0c8c3927a3c61ef67c4bf9253
kenlau <dknight1938@hotmail.com>
2018-04-02 00:02:38 +1000

    Fix the compilation of casino contract

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -1,13 +1,12 @@
-pragma solidity 0.4.20;
-
-import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
+pragma solidity ^0.4.20;
 
 contract Casino {
    address public owner;
    uint256 public minimumBet;
    uint256 public totalBet;
    uint256 public numberOfBets;
-   uint256 public maxAmountOfBets = 100;
+   uint256 public randomNumber;
+   uint256 public maxAmountOfBets = 10;
    address[] public players;
    
    struct Player {
@@ -15,25 +14,26 @@ contract Casino {
      uint256 numberSelected;
    }
    
-   mapping(address => Player) public playerInfo;
+   mapping(address => Player) playerInfo;
    
-   function Casino(uint256 _minimumBet) public {
+   function Casino(uint256 _minimumBet, uint256 _maxAmountOfBets) {
       owner = msg.sender;
       if (_minimumBet != 0) minimumBet = _minimumBet;
+      if (_maxAmountOfBets != 0)maxAmountOfBets = _maxAmountOfBets;
    }
    
    function kill() public {
       if(msg.sender == owner) selfdestruct(owner);
    }
    
-   function checkPlayerExists(address Player) {
+   function checkPlayerExists(address player) returns(bool) {
      for(uint256 i = 0;  i < players.length; i++) {
         if (players[i] == player) return true;
      }
      return false;
    }
    
-   function bet(uint256 numberSelected) public payable {
+   function bet(uint256 numberSelected) payable {
       require(!checkPlayerExists(msg.sender));
       require(numberSelected >= 1 && numberSelected <= 10);
       require(msg.value >= minimumBet);
@@ -43,19 +43,21 @@ contract Casino {
       numberOfBets++;
       players.push(msg.sender);
       totalBet += msg.value;
+      
+      if (numberOfBets >= maxAmountOfBets) generatedWinner();
    }
    
-   function generatedWinner() public {
-      uint256 randomNumber = block.number % 10 + 1;
-      givePrize(randomNumber);
+   function generatedWinner() {
+      randomNumber = block.number % 10 + 1;
+      givePrize();
    }
    
-   function givePrize(uint256 randomNumnber) {
+   function givePrize() {
       address[100] memory winners;
       uint256 count = 0;
       
       for (uint256 i = 0; i < players.length; i++) {
-        if (playerInfo[players[i]].numberSelected == randomNumnber) {
+        if (playerInfo[players[i]].numberSelected == randomNumber) {
           winners[count] = players[i];
         }
         delete playerInfo[players[i]];
@@ -66,9 +68,16 @@ contract Casino {
       
       for (uint256 j = 0; j < count; j++) {
         if(winners[j] != address(0)) {
-          winners[j].transfer(winnerEthAmount);
+          winners[j].transfer(winningEthAmount);
         }
       }
       
+      resetGame();
+   }
+   
+   function resetGame() {
+     players.length = 0;
+     totalBet = 0;
+     numberOfBets = 0;
    }
 }
